aadc50c4f407e1d2ab810d53aa87406a0f15027e
Murlocki <kirillkornilov03@gmail.com>
2024-09-16 00:27:46 +0300

    Lab 2.1. Добавлено чтение изображения с камеры и перевод его в hsv

---
+++ lab2/main.py
@@ -0,0 +1,13 @@
+import cv2
+def trackRed():
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret, frame = cap.read()
+        frameUpdate = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+        cv2.imshow('frame', frameUpdate)
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+trackRed()
