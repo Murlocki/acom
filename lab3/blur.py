029cf22ef1244286ce82074172b3b68d6f522854
Murlocki <kirillkornilov03@gmail.com>
2024-09-22 19:16:17 +0300

    Lab 3.2. Добавлено нормирование матрицы для получения элементов с суммой в 1

--- lab3/blur.py
+++ lab3/blur.py
@@ -1,17 +1,24 @@
 import numpy as np
 # Задание 1 Выполнить пункты 1 и 2 алгоритма, то есть построить
 # матрицу Гаусса. Просмотреть итоговую матрицу для размерностей 3, 5, 7
-def gaussMatrix(kernel_size, standard_deviation=0.2):
-    kernel = np.ones((kernel_size, kernel_size))
-    a = b = (kernel_size + 1) // 2
+def gaussMatrix(kernelSize, standardDeviation=0.2):
+    kernel = np.ones((kernelSize, kernelSize))
+    a = b = (kernelSize + 1) // 2
 
     # Строим матрицу свёртки
-    for i in range(kernel_size):
-        for j in range(kernel_size):
-            kernel[i, j] = gaussFunc(i, j, standard_deviation, a, b)
+    for i in range(kernelSize):
+        for j in range(kernelSize):
+            kernel[i, j] = gaussFunc(i, j, standardDeviation, a, b)
 
     print(kernel)
     print("//////////")
+
+    # Задание 2 Нормировать полученную матрицу Гаусса. Протестировать результаты на матрицах из предыдущего пункта.
+    # нормирование матрицы
+    # Находим сумму
+    elementSum = np.sum(kernel)
+    kernel = kernel/elementSum
+    print(kernel)
 def gaussFunc(x, y, omega, a, b):
     omega2 = 2 * omega ** 2
     m1 = 1 / (np.pi * omega2)
@@ -19,3 +26,5 @@ def gaussFunc(x, y, omega, a, b):
     return m1 * m2
 
 gaussMatrix(3,0.4)
+gaussMatrix(5,0.4)
+gaussMatrix(7,0.4)
